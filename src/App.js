8660b19d325a98e22f30b2044f7b110dfe190795
Mikayla Rogers <m.ikaylarogers@icloud.com>
2020-11-24 14:56:08 -0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+//Styles
 import './App.css';
+
+// Pages
+import Login from './containers/Login';
+import CreateAccount from './containers/CreateAccount';
+import UserProfile from './containers/UserProfile';
+
+// Components
+import Header from './components/Header';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY, // Repalce with .env file API reference
+  authDomain: "exercise-five-91f00.firebaseapp.com",
+  databaseURL: "https://exercise-five-91f00.firebaseio.com",
+  projectId: "exercise-five-91f00",
+  storageBucket: "exercise-five-91f00.appspot.com",
+  messagingSenderId: "179151166872",
+  appId: "1:179151166872:web:1c6045b85d001fa257dfed"
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header  />
+      <Router>
+
+        <Route exact path="/login">
+          <Login  />
+        </Route>
+
+        <Route exact path="/create-account">
+          <CreateAccount />
+        </Route>
+
+        <Route exact path="/">
+          <UserProfile  />
+        </Route>
+
+      </Router>
+
     </div>
   );
 }
+
 
 export default App;
